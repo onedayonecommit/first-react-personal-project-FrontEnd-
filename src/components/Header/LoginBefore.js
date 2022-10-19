30012ab5dee66618047b96c4a8d6b9045f306e78
onedayonecommit <rudghks0981@gmail.com>
2022-10-20 00:14:34 +0900

    회원가입 페이지 만들기

---
+++ src/components/Header/LoginBefore.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginBefore = () => {
+    return (
+        <div>LoginBefore</div>
+    )
+}
+
+export default LoginBefore
---
+++ src/page/Signup/Signuptest.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Signuptest = () => {
+    return (
+        <div>Signuptest</div>
+    )
+}
+
+export default Signuptest
