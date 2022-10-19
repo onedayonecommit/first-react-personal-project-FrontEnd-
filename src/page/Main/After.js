b11f7863f340c168ddb54496fc47b8b1e575f9f1
onedayonecommit <rudghks0981@gmail.com>
2022-10-20 00:14:19 +0900

    회원가입 페이지 만들기

--- src/page/Main/After.js
+++ src/page/Main/After.js
@@ -5,24 +5,7 @@ import { useNavigate } from 'react-router-dom'
 const After = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const statusNumber = useSelector(state => state.MainidCheck.status)
 
-    useEffect(() => {
-        console.log(statusNumber)
-        switch (statusNumber) {
-            case 0:
-                navigate("/")
-                break;
-            case 1:
-                navigate("/welcome/login")
-                break;
-            case 2:
-                navigate("/welcome/signup")
-                break;
-            default:
-                break;
-        }
-    }, [statusNumber])
     return (
         <div>
 
