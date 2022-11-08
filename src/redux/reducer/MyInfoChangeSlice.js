e399d0e1e5b3600510d4e6e2cdc0a45111f98fac
onedayonecommit <rudghks0981@gmail.com>
2022-11-08 23:42:30 +0900

    .

--- src/redux/reducer/MyInfoChangeSlice.js
+++ src/redux/reducer/MyInfoChangeSlice.js
@@ -49,3 +49,5 @@ const MyInfoChangeSlice = createSlice({
         })
     }
 })
+
+export { MyInfoChangeSlice }
--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
+import { MyInfoChangeSlice } from "./MyInfoChangeSlice";
 
 const rootReducer = combineReducers({
+    Myinfo: MyInfoChangeSlice
 });
 
 export default rootReducer;
